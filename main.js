f44eca6f11ce7cc5464ddb7274ba0f152f133627
wynand <wtheron2@yahoo.com>
2018-07-09 15:34:33 +0200

    created object and methods needed

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+const todo = {
+  todos: ['eet', 'code', 'slaap'],
 
+  displayTodos: function () {
+    console.log(this.todos);
+  },
+
+  addTodo: function (task) {
+    this.todos.push(task);
+    this.displayTodos();
+  },
+
+  changeTodo: function (index, newTask) {
+    this.todos[index] = newTask;
+    this.displayTodos();
+  },
+
+  deleteTodo: function (index) {
+    this.todos.splice(index, 1);
+    this.displayTodos();
+  },
+};
