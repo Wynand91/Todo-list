0d29fd1a51e3f21543c78d04d3e2863d65b6f28d
wynand <wtheron2@yahoo.com>
2018-07-19 15:07:38 +0200

    changed display method and added toggleAll method

--- main.js
+++ main.js
@@ -2,7 +2,20 @@ const todo = {
   todos: [],
 
   displayTodos: function () {
-    console.log(this.todos);
+
+    if (this.todos.length === 0) {
+      console.log("Your todo list is empty!");
+    } else {
+      for (let i = 0; i < this.todos.length; i++) {
+        if(this.todos[i].completed === true) {
+          console.log("(X) " + this.todos[i].todoText);
+        } else {
+          console.log("() " + this.todos[i].todoText);
+        }
+
+      }
+    }
+
   },
 
   addTodo: function (todoText) {
@@ -28,4 +41,31 @@ const todo = {
     this.todos.splice(index, 1);
     this.displayTodos();
   },
+
+  toggleAll: function() {
+    let totalTodos = this.todos.length;
+    let completedTodos = 0;
+
+    for (let i = 0; i < totalTodos; i++) {
+      if (this.todos[i].completed === true) {
+        completedTodos++;
+      }
+    }
+
+    //if all todos are completed, change all to false(not completed)
+    if (completedTodos === totalTodos) {
+      for (let i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = false;
+        }
+
+    //if all todos are not completed, change all to true(everything completed)
+    } else {
+      for (let i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = true;
+        }
+    }
+
+    this.displayTodos();
+
+  }
 };
